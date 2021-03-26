b2a5282f6acc210116aba5ad45bdc2b8ba19b8ac
Jimmie Harkins <ghana796@gmail.com>
2021-03-26 19:37:52 -0400

    deleted old website and added create-react-app base

--- js/topnav.js
+++
@@ -1,9 +0,0 @@
-function makeResponsive() {
-    var x = document.getElementById("menubar");
-    if (x.className === "menu") {
-      x.className += " responsive";
-    } else {
-      x.className = "menu";
-    }
-  }
-  

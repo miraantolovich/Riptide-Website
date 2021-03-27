c849cc720e3ac842e4b9d9ed5bf7b6ce7eb32578
Mira Antolovich <mla4783@rit.edu>
2021-03-26 21:46:31 -0400

    footer

--- src/App.js
+++ src/App.js
@@ -12,6 +12,25 @@ function App() {
         </nav>
         <img src="/assets/riptide-logo.png"></img>
       </header>
+      <body>
+      </body>
+      <footer>
+        <span>
+          <a href="https://twitter.com/">
+            <img src="/assets/twitter-icon.png"></img>@GIB_Studios
+          </a>
+        </span>
+        <span>
+          <a href="https://github.com/">
+            <img src="/assets/github-icon.png"></img>Project Riptide
+          </a>
+        </span>
+        <span>
+          <a href="mailto: riptide@gibstudios.com">
+            <img src="/assets/email-icon.png"></img>riptide@gibstudios.com
+          </a>
+        </span>
+      </footer>
     </div>
   );
 }

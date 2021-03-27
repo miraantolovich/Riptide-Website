8d1590db0e9f712c38c78154214e4e524c1c8169
Jimmie Harkins <ghana796@gmail.com>
2021-03-26 20:51:52 -0400

    began creating header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <nav>
+          <a href="/">Home</a>
+          <a href="/">About Us</a>
+          <a href="/">Updates</a>
+          <a href="/">Support</a>
+        </nav>
+        <img src="/assets/riptide-logo.png"></img>
       </header>
     </div>
   );
